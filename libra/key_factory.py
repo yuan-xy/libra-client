896aebb1d7613f11c06c2241a3f878c54311166e
yuan <yuanxinyu.hangzhou@gmail.com>
2019-09-12 23:42:39 +0800

    gen child key

--- libra/key_factory.py
+++ libra/key_factory.py
@@ -16,3 +16,27 @@ class KeyFactory:
         self.master = hmac.new(MASTER_KEY_SALT, seed, digestmod=hashlib.sha3_256).digest()
 
 
+    #See https://github.com/casebeer/python-hkdf/blob/master/hkdf.py
+    def hkdf_expand(self, pseudo_random_key, info=b"", length=32):
+        hash=hashlib.sha3_256
+        hash_len = hash().digest_size
+        length = int(length)
+        if length > 255 * hash_len:
+            raise Exception("Cannot expand to more than 255 * %d = %d bytes using the specified hash function" %\
+                (hash_len, 255 * hash_len))
+        blocks_needed = length // hash_len + (0 if length % hash_len == 0 else 1) # ceil
+        okm = b""
+        output_block = b""
+        for counter in range(blocks_needed):
+            output_block = hmac.new(pseudo_random_key, output_block + info + bytearray((counter + 1,)),\
+                hash).digest()
+            okm += output_block
+        return okm[:length]
+
+    def private_child(self, child_number):
+        INFO_PREFIX = b"LIBRA WALLET: derived key$"
+        info = INFO_PREFIX + child_number.to_bytes(8, "little")
+        hkdf_expand = self.hkdf_expand(self.master, info, 32)
+        return hkdf_expand
+
+
--- test/test_key_factory.py
+++ test/test_key_factory.py
@@ -13,7 +13,13 @@ def test_key():
     kfac = libra.KeyFactory(seed)
     master = bytes.hex(kfac.master)
     assert master == "16274c9618ed59177ca948529c1884ba65c57984d562ec2b4e5aa1ee3e3903be"
-
-
+    child0 = kfac.private_child(0)
+    assert bytes.hex(child0) == "358a375f36d74c30b7f3299b62d712b307725938f8cc931100fbd10a434fc8b9"
+    child1 = kfac.private_child(1)
+    assert bytes.hex(child1) == "a325fe7d27b1b49f191cc03525951fec41b6ffa2d4b3007bb1d9dd353b7e56a6"
+    child0_again = kfac.private_child(0)
+    assert child0_again == child0
+    child1_again = kfac.private_child(1)
+    assert child1_again == child1
 
 
