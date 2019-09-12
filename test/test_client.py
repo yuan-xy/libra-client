c20a28082a52a0f2974f89ade05989e15e017da2
yuan <yuanxinyu.hangzhou@gmail.com>
2019-09-12 11:47:59 +0800

    add get transaction test

--- test/test_client.py
+++ test/test_client.py
@@ -10,9 +10,8 @@ def test_events():
 
 def test_get_transaction():
     c = libra.Client("testnet")
-    txn = c.get_transaction(8149)
-    pdb.set_trace()
-    assert txn != None
+    txn = c.get_transaction(1)
+    assert len(txn.raw_txn_bytes) > 0
 
 def test_get_latest_transaction_version():
     c = libra.Client("testnet")
@@ -24,3 +23,10 @@ def test_get_balance():
     c = libra.Client("testnet")
     balance = c.get_balance(address)
     assert balance > 0
+
+def test_get_account_transaction():
+    address = "000000000000000000000000000000000000000000000000000000000a550c18"
+    c = libra.Client("testnet")
+    #pdb.set_trace()
+    txn = c.get_account_transaction(address, 6600, True)
+    assert len(txn.raw_txn_bytes) > 0
