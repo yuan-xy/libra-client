ec7c6e8f94417cb2ee66500ed719f795c77ce6c4
yuan <yuanxinyu.hangzhou@gmail.com>
2019-09-16 22:54:37 +0800

    remove magic byte code

--- libra/transaction.py
+++ libra/transaction.py
@@ -3,8 +3,6 @@ from libra.proto.transaction_pb2 import TransactionArgument
 import libra
 from datetime import datetime
 
-PEER_TO_PEER_TXN = '4c49425241564d0a010007014a00000004000000034e000000060000000c54000000060000000d5a0000000600000005600000002900000004890000002000000007a90000000f00000000000001000200010300020002040200030003020402063c53454c463e0c4c696272614163636f756e74046d61696e0f7061795f66726f6d5f73656e6465720000000000000000000000000000000000000000000000000000000000000000000100020104000c000c0113010002'
-
 class Transaction:
     def to_raw_tx_proto(
         self, sender, sequence_number, max_gas_amount=140_000, gas_unit_price=0, txn_expiration=100
@@ -26,7 +24,7 @@ class Transaction:
         self.arg_vals = arg_vals
 
     def fill_program(self, program):
-        program.code = bytes.fromhex(self.opcodes)
+        program.code = self.opcodes
         for kind, val in zip(self.arg_types, self.arg_vals):
             arg = program.arguments.add()
             if kind.lower() == "address":
@@ -42,7 +40,15 @@ class Transaction:
 
     @classmethod
     def gen_transfer_transaction(cls, receiver, value):
-        tran = cls(PEER_TO_PEER_TXN, ["address", "u64"], [receiver, value])
+        code = get_script_bytecode("transaction_scripts/peer_to_peer_transfer.bytecode")
+        tran = cls(code, ["address", "u64"], [receiver, value])
         return tran
+
+    @staticmethod
+    def get_script_bytecode(script_file):
+        with open(script_file) as f:
+            data = f.read()
+            amap = eval(data)
+            return bytes(amap['code'])
 
 
