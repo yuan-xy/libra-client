1b3307b31a80d79275a361e4880d0e8109ac0159
Yuan xy <yuan_xin_yu@hotmail.com>
2019-10-12 07:51:10 +0800

    move compiler no longer support -o option and the suffix of bytecode file is mv

--- test/test_bytecode.py
+++ test/test_bytecode.py
@@ -2,8 +2,14 @@ from libra.bytecode import *
 import pytest
 import pdb
 
-def test_get_code_by_filenamet():
-    code = get_code_by_filename("transaction_scripts/mint.bytecode")
-    assert code == bytecode["mint"]
-    assert get_transaction_name(code) == "mint_transaction"
+def check_bytecode(name):
+    code = get_code_by_filename(f"transaction_scripts/{name}.mv")
+    assert code == bytecode[name]
+    assert get_transaction_name(code) == f"{name}_transaction"
+
+def test_get_code_by_filename():
+    check_bytecode("mint")
+    check_bytecode("peer_to_peer_transfer")
+    check_bytecode("create_account")
+    check_bytecode("rotate_authentication_key")
 
--- transaction_scripts/gen_transaction_bytecode.sh
+++ transaction_scripts/gen_transaction_bytecode.sh
@@ -1,4 +1,4 @@
-cargo run -p compiler -- -o mint.bytecode ./language/stdlib/transaction_scripts/mint.mvir
-cargo run -p compiler -- -o peer_to_peer_transfer.bytecode ./language/stdlib/transaction_scripts/peer_to_peer_transfer.mvir
-cargo run -p compiler -- -o rotate_authentication_key.bytecode ./language/stdlib/transaction_scripts/rotate_authentication_key.mvir
-cargo run -p compiler -- -o create_account.bytecode ./language/stdlib/transaction_scripts/create_account.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/mint.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/peer_to_peer_transfer.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/rotate_authentication_key.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/create_account.mvir
