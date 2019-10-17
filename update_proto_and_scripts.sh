d5d5509335c113fe076d37b8bcc23ff0846dccf0
Yuan xy <yuan_xin_yu@hotmail.com>
2019-10-17 17:40:00 +0800

    move compiler no longer support -o option and the suffix of bytecode file is mv

--- update_proto_and_scripts.sh
+++ update_proto_and_scripts.sh
@@ -6,16 +6,17 @@ git clone https://github.com/libra/libra.git
 cd libra
 git checkout origin/testnet
 
-cp ./scripts/cli/consensus_peers.config.toml ../libra-client/
+cp ./scripts/cli/consensus_peers.config.toml ../../libra-client/libra
+
+../../libra-client/transaction_scripts/gen_transaction_bytecode.sh
+cp ./language/stdlib/transaction_scripts/*.mv ../../libra-client/transaction_scripts/
+#scp xn@an1:~/libra/
 
 rm ../../libra-client/proto/*.proto
 find . -name *.proto | xargs cp -t ../../libra-client/proto/
 cd ../../libra-client/
 rpl "shared/mempool_status" "mempool_status" proto/mempool.proto
 
-../../libra-client/transaction_scripts/gen_transaction_bytecode.sh
-cp *.bytecode ../../libra-client/transaction_scripts/
-#scp xn@an1:~/libra/
 
 cd ../../libra-client/
 ./generate_protobuf.sh
