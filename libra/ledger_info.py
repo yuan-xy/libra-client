1040537a63a4c780edda608c926d56484990ed50
Yuan xy <yuan_xin_yu@hotmail.com>
2019-10-26 17:23:11 +0800

    change epoch_num to epoch

--- libra/ledger_info.py
+++ libra/ledger_info.py
@@ -13,7 +13,7 @@ class LedgerInfo(Struct):
         ('transaction_accumulator_hash', [Uint8]),
         ('consensus_data_hash', [Uint8]),
         ('consensus_block_id', [Uint8]),
-        ('epoch_num', Uint64),
+        ('epoch', Uint64),
         ('timestamp_usecs', Uint64),
         ('next_validator_set', OptionValidatorSet)
     ]
@@ -30,7 +30,7 @@ class LedgerInfo(Struct):
         ret.transaction_accumulator_hash = bytes_to_int_list(proto.transaction_accumulator_hash)
         ret.consensus_data_hash = bytes_to_int_list(proto.consensus_data_hash)
         ret.consensus_block_id = bytes_to_int_list(proto.consensus_block_id)
-        ret.epoch_num = proto.epoch_num
+        ret.epoch = proto.epoch
         ret.timestamp_usecs = proto.timestamp_usecs
         if proto.HasField("next_validator_set"):
             vset = ValidatorSet.from_proto(proto.next_validator_set)
