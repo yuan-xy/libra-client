37e46440082906e1410fa61645c5fea6beeade24
Yuan xy <yuan_xin_yu@hotmail.com>
2019-10-30 23:17:29 +0800

    test_event_received

--- test/test_events.py
+++ test/test_events.py
@@ -3,11 +3,12 @@ from libra.event import *
 import libra
 import pdb
 
-def test_event():
+def test_event_sent():
     address = libra.AccountConfig.association_address()
     c = libra.Client("testnet")
     events = c.get_latest_events_sent(address, 2)
     assert len(events) == 2
+    assert events[0].transaction_version >= events[1].transaction_version
     contracts = [ContractEvent.from_proto(x.event) for x in events]
     assert contracts[0].key == contracts[1].key
     assert contracts[0].sequence_number-1 == contracts[1].sequence_number
@@ -17,3 +18,21 @@ def test_event():
     assert len(aes[0].account) == 32
     assert aes[1].amount >0
     assert len(aes[1].account) == 32
+    res = c.get_account_resource(address)
+    assert res.sent_events.key == contracts[0].key
+    assert res.sent_events.count == contracts[0].sequence_number+1
+
+def test_event_received():
+    address = libra.AccountConfig.association_address()
+    c = libra.Client("testnet")
+    events = c.get_latest_events_received(address, 1)
+    assert len(events) == 1
+    assert events[0].transaction_version > 0
+    contracts = [ContractEvent.from_proto(x.event) for x in events]
+    assert len(contracts[0].event_data) == 44
+    aes = [AccountEvent.deserialize(x.event_data) for x in contracts]
+    assert aes[0].amount >0
+    assert len(aes[0].account) == 32
+    res = c.get_account_resource(address)
+    assert res.received_events.key == contracts[0].key
+    assert res.received_events.count == contracts[0].sequence_number+1
