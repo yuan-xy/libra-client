047605c05af0873c62253a5f6a3aec48e1ef39bf
Yuan xy <yuan_xin_yu@hotmail.com>
2019-11-26 13:07:41 +0800

    remove transaction.program

--- libra/transaction/transaction_payload.py
+++ libra/transaction/transaction_payload.py
@@ -1,5 +1,4 @@
 from canoser import RustEnum
-from libra.transaction.program import Program
 from libra.transaction.write_set import WriteSet
 from libra.transaction.script import Script
 from libra.transaction.module import Module
