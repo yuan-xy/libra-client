62e63b6ef7a44da6396c86d5da2dcc24333551bb
yuan_xy <yuan_xin_yu@hotmail.com>
2019-12-29 23:09:32 +0800

    gen_transaction_bytecode.sh

--- transaction_scripts/gen_transaction_bytecode.sh
+++ transaction_scripts/gen_transaction_bytecode.sh
@@ -1,4 +1,20 @@
-cargo run -p compiler --  ./language/stdlib/transaction_scripts/mint.mvir
+cd ../libra/
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/add_validator.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/remove_validator.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/register_validator.mvir
 cargo run -p compiler --  ./language/stdlib/transaction_scripts/peer_to_peer_transfer.mvir
-cargo run -p compiler --  ./language/stdlib/transaction_scripts/rotate_authentication_key.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/peer_to_peer_transfer_with_metadata.mvir
 cargo run -p compiler --  ./language/stdlib/transaction_scripts/create_account.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/mint.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/rotate_authentication_key.mvir
+cargo run -p compiler --  ./language/stdlib/transaction_scripts/rotate_consensus_pubkey.mvir
+
+mv  ./language/stdlib/transaction_scripts/add_validator.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/remove_validator.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/register_validator.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/peer_to_peer_transfer.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/peer_to_peer_transfer_with_metadata.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/create_account.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/mint.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/rotate_authentication_key.mv ../libra-client/transaction_scripts/
+mv  ./language/stdlib/transaction_scripts/rotate_consensus_pubkey.mv ../libra-client/transaction_scripts/
