1c469b146ee418d96d3a6c412b0296a4baf3fb22
yuan_xy <yuan_xin_yu@hotmail.com>
2020-02-08 18:36:15 +0800

    update pip packages and requirements.txt

---
+++ scripts/update_requirements.sh
@@ -0,0 +1,3 @@
+#pip3 install --upgrade pip
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+
--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ with open("libra_client/version.py", "r") as fp:
 
 
 install_requires=[
-        'canoser>=0.7.6',
-        'libra-core>=0.7.6',
+        'canoser>=0.7.7',
+        'libra-core>=0.7.7',
         'protobuf',
         'grpcio',
         'more-itertools',
